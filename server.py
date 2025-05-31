723b77f12609479515151945d8fd2f8fdb88a02a
pro-code001 <fragmenfragmen777@gmail.com>
2025-05-31 12:07:39 +0500

    server.py

--- server.py
+++ server.py
@@ -57,4 +57,5 @@ def handle_disconnect():
             break
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=10000, allow_unsafe_werkzeug=True)
+
