881177b362682df57d0177f0aaf64c9890a2a032
pro-code001 <fragmenfragmen777@gmail.com>
2025-05-31 07:57:14 +0500

    server.py

--- server.py
+++ server.py
@@ -5,13 +5,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret123'
 socketio = SocketIO(app)
 
-chat_rooms = {}
+chat_rooms = {}  # Xonalar va parollari saqlanadi
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Bu yerda index.html sahifasini yuboradi
+    return render_template('index.html')  # templates/index.html ni render qiladi
 
-# Quyidagi SocketIO eventlar sizniki bo‘lishi kerak
+@socketio.on('create')
+def handle_create(data):
+    room = data['room']
+    password = data['password']
+    username = data['username']
+
+    if room in chat_rooms:
+        socketio.emit('error', {'msg': 'Room already exists!'}, room=request.sid)
+    else:
+        chat_rooms[room] = password
+        join_room(room)
+        send(f"{username} created and joined the room {room}", room=room)
+        socketio.emit('created', {'room': room}, room=request.sid)
+
 @socketio.on('join')
 def handle_join(data):
     room = data['room']
@@ -19,18 +32,19 @@ def handle_join(data):
     username = data['username']
 
     if room not in chat_rooms:
-        chat_rooms[room] = password
-        join_room(room)
-        send(f"{username} xonaga qo'shildi", room=room)
+        socketio.emit('error', {'msg': 'Room does not exist!'}, room=request.sid)
     elif chat_rooms[room] == password:
         join_room(room)
-        send(f"{username} xonaga qo'shildi", room=room)
+        send(f"{username} joined the room {room}", room=room)
+        socketio.emit('join', {'room': room}, room=request.sid)
     else:
-        socketio.emit('error', {'msg': 'Parol noto‘g‘ri!'}, room=request.sid)
+        socketio.emit('error', {'msg': 'Incorrect password!'}, room=request.sid)
 
 @socketio.on('message')
 def handle_message(data):
-    send(data['msg'], room=data['room'])
+    room = data['room']
+    msg = data['msg']
+    send(msg, room=room)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
