8357910baf16300a8168ca1f5620ac9580872bd9
pro-code001 <fragmenfragmen777@gmail.com>
2025-05-31 14:20:07 +0500

    server.py

--- server.py
+++ server.py
@@ -6,45 +6,37 @@ app.config['SECRET_KEY'] = 'secret123'
 socketio = SocketIO(app)
 
 chat_rooms = {}  # {room_name: password}
+users = {}       # {username: password} - oddiy foydalanuvchi bazasi
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('create')
-def handle_create(data):
-    room = data['room']
-    password = data['password']
+@socketio.on('signup')
+def handle_signup(data):
     username = data['username']
-
-    if room in chat_rooms:
-        socketio.emit('error', {'msg': 'Room already exists!'}, room=request.sid)
-    else:
-        chat_rooms[room] = password
-        join_room(room)
-        send(f"{username} created and joined the room {room}", room=room)
-        socketio.emit('created', {'room': room}, room=request.sid)
-
-@socketio.on('join')
-def handle_join(data):
-    room = data['room']
     password = data['password']
-    username = data['username']
 
-    if room not in chat_rooms:
-        socketio.emit('error', {'msg': 'Room does not exist!'}, room=request.sid)
-    elif chat_rooms[room] == password:
-        join_room(room)
-        send(f"{username} joined the room {room}", room=room)
-        socketio.emit('joined', {'room': room}, room=request.sid)
+    if username in users:
+        socketio.emit('signup_response', {'success': False, 'msg': 'Username already taken!'}, room=request.sid)
     else:
-        socketio.emit('error', {'msg': 'Incorrect password!'}, room=request.sid)
+        users[username] = password
+        socketio.emit('signup_response', {'success': True, 'msg': 'Sign up successful!'}, room=request.sid)
 
-@socketio.on('message')
-def handle_message(data):
-    room = data['room']
-    msg = data['msg']
-    send(msg, room=room)
+@socketio.on('login')
+def handle_login(data):
+    username = data['username']
+    password = data['password']
+
+    if username not in users:
+        socketio.emit('login_response', {'success': False, 'msg': 'User does not exist!'}, room=request.sid)
+    elif users[username] != password:
+        socketio.emit('login_response', {'success': False, 'msg': 'Incorrect password!'}, room=request.sid)
+    else:
+        socketio.emit('login_response', {'success': True, 'msg': 'Login successful!'}, room=request.sid)
+
+# Chat room handlers (create, join, message) shu yerda qoladi
+# Qolgan kodlarni o'zgartirish shart emas, lekin siz xohlasangiz foydalanuvchi login bo'lmasa chatga kira olmasin deb qo'shishingiz mumkin.
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
