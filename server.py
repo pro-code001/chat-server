481c345edd382bbcb9ea41e3e094568e059f58e6
pro-code001 <fragmenfragmen777@gmail.com>
2025-05-31 12:40:42 +0500

    server.py

--- server.py
+++ server.py
@@ -48,4 +48,6 @@ def handle_message(data):
     emit('message', msg, to=room)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", port=10000, allow_unsafe_werkzeug=True)
+
