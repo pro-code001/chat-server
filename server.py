e679e0ff3917c28472299f6a6818b1d757f1db71
pro-code001 <fragmenfragmen777@gmail.com>
2025-05-31 12:31:41 +0500

    server.py

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, join_room, send
+from flask_socketio import SocketIO, join_room, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret123'
 socketio = SocketIO(app)
 
-chat_rooms = {}  # {room_name: password}
+rooms = {}
 
 @app.route('/')
 def index():
@@ -17,13 +16,13 @@ def handle_create(data):
     password = data['password']
     username = data['username']
 
-    if room in chat_rooms:
-        socketio.emit('error', {'msg': 'Room already exists!'}, room=request.sid)
-    else:
-        chat_rooms[room] = password
-        join_room(room)
-        send(f"{username} created and joined the room {room}", room=room)
-        socketio.emit('created', {'room': room}, room=request.sid)
+    if room in rooms:
+        emit('error', {'msg': 'Room already exists.'})
+        return
+
+    rooms[room] = {'password': password}
+    join_room(room)
+    emit('created', {'room': room})
 
 @socketio.on('join')
 def handle_join(data):
@@ -31,20 +30,22 @@ def handle_join(data):
     password = data['password']
     username = data['username']
 
-    if room not in chat_rooms:
-        socketio.emit('error', {'msg': 'Room does not exist!'}, room=request.sid)
-    elif chat_rooms[room] == password:
-        join_room(room)
-        send(f"{username} joined the room {room}", room=room)
-        socketio.emit('joined', {'room': room}, room=request.sid)
-    else:
-        socketio.emit('error', {'msg': 'Incorrect password!'}, room=request.sid)
+    if room not in rooms:
+        emit('error', {'msg': 'Room does not exist.'})
+        return
+
+    if rooms[room]['password'] != password:
+        emit('error', {'msg': 'Incorrect password.'})
+        return
+
+    join_room(room)
+    emit('joined', {'room': room})
 
 @socketio.on('message')
 def handle_message(data):
     room = data['room']
     msg = data['msg']
-    send(msg, room=room)
+    emit('message', msg, to=room)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+    socketio.run(app)
